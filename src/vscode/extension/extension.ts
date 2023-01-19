80cad90d727f83b1a29de81fc6b75a4860f785df
sfoslund <sfoslund@microsoft.com>
2023-01-19 10:03:16 -0800

    Launching browser

--- src/vscode/extension/extension.ts
+++ src/vscode/extension/extension.ts
@@ -6,6 +6,8 @@ import { createDefaultPromiseFactory } from '../../common/promises/promise-facto
 import { createDefaultLogger } from '../../common/logging/default-logger';
 import { ChromiumAdapter } from '../../common/browser-adapters/chromium-adapter';
 import { PassthroughBrowserEventManager } from 'common/browser-adapters/passthrough-browser-event-manager';
+// const CDP = require('chrome-remote-interface');
+import puppeteer from 'puppeteer-core';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Activated');
@@ -24,7 +26,8 @@ export function activate(context: vscode.ExtensionContext) {
         }
 
 		vscode.window.showInformationMessage('Injecting scripts into tab with ID: ' + tabId);
-		makeCdpConnection(tabId);
+		await setUpBrowserInstance(tabId);
+		// await makeCdpConnection(tabId);
 		// injectScripts(tabId); // TODO uncommenting this causes activation error
 
 		// TODO possibly another command -> provide a url and open it in a webview?
@@ -43,7 +46,7 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
-// TODO just uncommenting this fails
+// TODO just uncommenting this fails-> can only be loaded in browser
 // function injectScripts(tabId: number) {
 // 	const browserEventManager = new PassthroughBrowserEventManager();
 //     const browserAdapter = new ChromiumAdapter(browserEventManager);
@@ -53,9 +56,91 @@ export function activate(context: vscode.ExtensionContext) {
 //     injector.injectScripts(tabId);
 // }
 
+// Attempting to connect to existing browser instance with CDP-> not working, unable to connect/ socket hang up
+// async function makeCdpConnection(tabId: number) {
+//     let client;
+//     try {
+//         // connect to endpoint
+// 		console.log("Awaiting CDP")
+//         client = await CDP({host: '142.251.33.78', port: '443'});
+// 		console.log("Got cdp")
+//         // extract domains
+//         const {Network, Page} = client;
+//         // setup handlers
+//         Network.requestWillBeSent((params: { request: { url: any; }; }) => {
+//             console.log(params.request.url);
+//         });
+// 		console.log("Set up handler")
+//         // enable events then start!
+//         await Network.enable();
+//         await Page.enable();
+//         await Page.navigate({url: 'https://github.com'});
+//         await Page.loadEventFired();
+// 		console.log("started")
+//     } catch (err) {
+//         console.error(err);
+//     } finally {
+//         if (client) {
+//             await client.close();
+//         }
+//     }
+// }
 
-function makeCdpConnection(tabId: number) {
+async function setUpBrowserInstance(tabId: number) {
+	const url = 'https://github.com';
+    const { hostname, port, defaultUrl, userDataDir } = getRemoteEndpointSettings();
+	const browserPath = await getBrowserPath();
+	const browserInstance = await launchBrowser(browserPath, port, url, userDataDir);
+}
 
+export async function launchBrowser(browserPath: string, port: number, targetUrl: string, userDataDir?: string): Promise<puppeteer.Browser> {
+    const args = [
+        '--no-first-run',
+        '--no-default-browser-check',
+        `--remote-debugging-port=${port}`,
+        targetUrl,
+    ];
+
+    const headless: boolean = false;
+
+    let browserArgs: string[] = [];
+    browserArgs = browserArgs.filter(arg => !arg.startsWith('--remote-debugging-port') && arg !== targetUrl);
+
+    if (userDataDir) {
+        args.unshift(`--user-data-dir=${userDataDir}`);
+        browserArgs = browserArgs.filter(arg => !arg.startsWith('--user-data-dir'));
+    }
+
+    if (browserArgs.length) {
+        args.unshift(...browserArgs);
+    }
+
+    const browserInstance = await puppeteer.launch({executablePath: browserPath, args, headless});
+    return browserInstance;
+}
+
+
+export interface IDevToolsSettings {
+    hostname: string;
+    port: number;
+    useHttps: boolean;
+    defaultUrl: string;
+    userDataDir: string;
+    timeout: number;
+}
+
+export function getRemoteEndpointSettings(): IDevToolsSettings {
+    const hostname: string = 'localhost';
+    const port: number = 9222;
+    const useHttps: boolean = false;
+    const defaultUrl: string = 'http://localhost:3000/';
+    const timeout: number = 10000;
+
+    return { hostname, port, useHttps, defaultUrl, userDataDir: '', timeout };
+}
+
+export async function getBrowserPath(): Promise<string> {
+    return 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe';
 }
 
 export function deactivate() {}
