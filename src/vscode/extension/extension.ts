cc979ed41bdfe9a650b807f01c064640b3ceda5d
sfoslund <sfoslund@microsoft.com>
2023-02-16 12:50:13 -0800

    WIP show violations

--- src/vscode/extension/extension.ts
+++ src/vscode/extension/extension.ts
@@ -3,6 +3,7 @@
 import * as vscode from 'vscode';
 import puppeteer, { Browser, Page } from 'puppeteer-core';
 import { isEmpty } from 'lodash';
+import path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('accessibility-insights.automated-checks', async () => {
@@ -19,6 +20,7 @@ export function activate(context: vscode.ExtensionContext) {
         var axeRunResults = await runAutomatedChecks(page);
         console.log(JSON.stringify(axeRunResults))
         vscode.window.showInformationMessage("Finished running axe " + axeRunResults.value.testEngine.version + " against " + axeRunResults.value.url)
+        showViolations(axeRunResults.value.violations) 
 	});
 
 	context.subscriptions.push(disposable);
@@ -83,15 +85,51 @@ export async function getBrowserPath(): Promise<string> {
     return 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe';
 }
 
+const rootContainerId = 'accessibility-insights-root-container';
+
 async function injectScripts(browserInstance: Browser): Promise<void> {
     try {
         const pages = await browserInstance.pages();
         const page = pages[0];
         await injectAxeIfUndefined(page);
-        // await page.addStyleTag({path: 'C:/code/accessibility-insights-web/dist/src/injected/styles/injected.css'});
+        var injectedPath = path.join(__dirname, '../../extension/devBundle/injected.css');
+        await page.addStyleTag({path: injectedPath});
+        await page.evaluate((id) => {
+            const root = document.createElement('div');
+            root.id = id;
+            document.body.append(root);
+        }, rootContainerId)
+        await createShadowHost(page);
+        const shadowHostElement = await page.$(`#insights-shadow-host`);
+        await page.evaluate((shadowHostElement, pathName) => {
+            const shadow = shadowHostElement!.attachShadow({mode: 'open'});
+            const container = document.createElement('div');
+            container.id = 'insights-shadow-container';
+            shadow.append(container);
+            const shadowContainer = shadow.firstChild as HTMLElement;
+            const styleElement = document.createElement('link');
+            styleElement.rel = 'stylesheet';
+            styleElement.href = pathName;
+            styleElement.type = 'text/css';
+            shadowContainer.appendChild(styleElement);
+            return shadowContainer;
+        }, shadowHostElement, injectedPath)
     }catch(error){
         console.log(error)
     }
+}
+
+async function createShadowHost(page: Page): Promise<HTMLElement> {
+    return await page.evaluate((rootContainerId) => {
+        const rootContainer = document.getElementById(rootContainerId)
+        if (rootContainer == null) {
+            throw Error('expected rootContainer to be defined and not null');
+        }
+        const host = document.createElement('div');
+        host.id = 'insights-shadow-host';
+        rootContainer.append(host);
+        return host;
+    }, rootContainerId)
 }
 
 async function injectAxeIfUndefined(page: Page): Promise<void> {
@@ -131,6 +169,18 @@ function handleException(error: any): void {
         console.log(error);
         vscode.window.showErrorMessage("Error running automated checks: " + error)
     }
+}
+
+function showViolations(violations: any) {
+    const diagnosticsCollection = vscode.languages.createDiagnosticCollection('Accessibility Insights');
+
+    const diagResults: vscode.Diagnostic[] = violations.map((violation: { id: any; help: any; }) => {
+        return { 
+            code: `Accessibility Insights (${violation.id})`,
+            message: violation.help,
+            severity: vscode.DiagnosticSeverity.Error,
+        } as vscode.Diagnostic});
+    diagnosticsCollection.set(vscode.Uri.parse('C:/code/accessibility-insights-web/test.ts'), diagResults);
 }
 
 export function deactivate() {}
